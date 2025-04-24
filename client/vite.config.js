266745b301b526a8c1bfa7d100b2dae8b39d10c7
iamkartikbhardwaj <bkartik05@gmail.com>
2025-04-24 22:20:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/treatment-stages": {
-        target: "https://rilcet.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/evaluation": {
-        target: "https://rilcet.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
